88f929aba0fff9b843b40fc2a1a24933c9e8b8d3
Manoj Kumar Binwal <114239126+ManojBinwal@users.noreply.github.com>
2023-08-14 12:26:24 +0530

    using destructuring in hook

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 import React, {useState} from 'react';
 
 function App() {
-  const emailState = useState('a@a.com');
-  const email = emailState[0];
-  const setEmail = emailState[1];
+  // const emailState = useState('a@a.com');
+  // const email = emailState[0];
+  // const setEmail = emailState[1];
 
-  const nameState = useState('Manoj');
-  const name = nameState[0];
-  const setName = nameState[1];
+  // const nameState = useState('Manoj');
+  // const name = nameState[0];
+  // const setName = nameState[1];
+
+  const [email,setEmail] = useState('');
+  const [name,setName] = useState('');
 
   function handleChange (e) {
     setEmail(e.target.value)
